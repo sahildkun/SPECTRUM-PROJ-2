d810c7f33c1ff107def13c75e84362256fbe1995
Jyotishree <jyotishreeb007@gmail.com>
2022-11-02 19:53:55 +0530

    add logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -61,11 +61,11 @@ function Footer() {
           </li>
         </nav>
       </div>
-      <div class="px-2 py-4 text-left">
+      <div class="lg:w-1/4 md:w-1/2 w-full py-4 text-left ml-14 mb-3 ">
       <div class="title-font font-medium text-white tracking-widest text-lg mb-5">GET IN TOUCH</div>
         <p class="flex items-center justify-center text-md md:justify-start mb-3 text-gray-400 hover:text-gray-600">
            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home"
-            class=" hidden md:block w-5 mr-3 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 670 560">
+            class=" hidden md:block w-7 mr-3 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 540 650">
             <path fill="currentColor"
               d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z">
             </path>
