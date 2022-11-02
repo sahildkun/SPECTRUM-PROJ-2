98c347d57286a2d48570c7c97fe7a6a83573f9dc
sahildkun <sahilsahoo23@gmail.com>
2022-11-02 23:09:47 +0530

    apun ka hogaya

--- src/Data.js
+++ src/Data.js
@@ -15,7 +15,7 @@ export const gallery =  [
     {
 
         id:2,
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" ,
+        image: "https://images.pexels.com/photos/40784/drops-of-water-water-nature-liquid-40784.jpeg?cs=srgb&dl=pexels-pixabay-40784.jpg&fm=jpg" ,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
         category: "water",
         artist: "eren yeager"
@@ -25,7 +25,7 @@ export const gallery =  [
     {
 
         id:3,
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" ,
+        image: "https://i.pinimg.com/236x/8d/f9/4d/8df94df87425ef1ff80273a719fcb999.jpg" ,
         
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
         category: "dark",
@@ -46,7 +46,7 @@ export const gallery =  [
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" ,
         
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
-        category: "dark",
+        category: "oil",
         artist: "eren yeager"
     },
     {
@@ -55,7 +55,7 @@ export const gallery =  [
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" ,
         
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
-        category: "dark",
+        category: "water",
         artist: "eren yeager"
     }
 ]
--- src/Gallery.js
+++ src/Gallery.js
@@ -5,9 +5,32 @@ import Navbar2 from './Navbar2';
 function Gallery() {
 
   const [art, setArt] = useState(gallery);
+
+   const filterItem = (categ) => {
+    const updateList = gallery.filter((elem)=>{
+      return elem.category === categ;
+    })
+
+    setArt(updateList);
+   }
   return (
     <>
     <Navbar2/>
+    {/* intro */}
+
+    <div className='text-4xl text-center my-20'>
+      GALLERY 
+    </div>
+
+    <div className='flex flex-row justify-center space-x-10 mb-10'>
+      <button className='border-2 p-3 bg-black text-white' onClick={()=> filterItem('oil')}>OIL</button>
+      <button className='border-2 p-3 bg-black text-white' onClick={()=> filterItem('water')}>WATER</button>
+      <button className='border-2 p-3 bg-black text-white'onClick={()=> filterItem('dark')}>DARK</button>
+      <button className='border-2 p-3 bg-black text-white'onClick={()=> setArt(gallery)}>All</button>
+      
+    </div>
+
+
     <div className='grid grid-cols-4 gap-3'>
     {
       art.map((elem)=>{
