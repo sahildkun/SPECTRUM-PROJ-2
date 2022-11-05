d7261e1001f12ee6fcb043dac319c84646bf0a85
Jyotishree <jyotishreeb007@gmail.com>
2022-11-05 19:30:35 +0530

    data added

--- src/Data.js
+++ src/Data.js
@@ -5,57 +5,291 @@ export const gallery =  [
     {
 
         id:1,
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" ,
+        image: "https://w0.peakpx.com/wallpaper/317/526/HD-wallpaper-abstract-art-blue-grey-orange-red-s7-s8-silver.jpg" ,
         
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
-        category: "oil",
-        artist: "eren yeager"
+        description: "Abstraction demonstrate an escape route from reality in depicting images in the artwork.",
+        category: "Abstract",
+        artist: "Maxwell Apollo"
     },
 
     {
 
         id:2,
-        image: "https://images.pexels.com/photos/40784/drops-of-water-water-nature-liquid-40784.jpeg?cs=srgb&dl=pexels-pixabay-40784.jpg&fm=jpg" ,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
-        category: "water",
-        artist: "eren yeager"
+        image: "https://wallpaperaccess.com/full/1836113.jpg" ,
+        description: "Fusion of the precision of geometry and the fluidity that arises from intuition and automatism.",
+        category: "Abstract",
+        artist: "Connor Stark"
     },
 
 
     {
 
         id:3,
-        image: "https://i.pinimg.com/236x/8d/f9/4d/8df94df87425ef1ff80273a719fcb999.jpg" ,
+        image: "https://wallpaperaccess.com/full/2855743.jpg" ,
         
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
-        category: "dark",
-        artist: "eren yeager"
+        description: "Abstraction demonstrate an escape route from reality in depicting images in the artwork.",
+        category: "Abstract",
+        artist: "Christian Dusk"
     },
     {
 
         id:4,
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" ,
+        image: "https://wallpaperaccess.com/full/53932.jpg" ,
         
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
-        category: "dark",
-        artist: "eren yeager"
+        description: "It has a superb sense of composition, design, and contrast and conveys a remarkable sense of balance and harmony.",
+        category: "Abstract",
+        artist: "Bobby Douglas"
     },
     {
 
         id:5,
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" ,
+        image: "https://wallpaperaccess.com/full/6934122.jpg" ,
         
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
-        category: "oil",
-        artist: "eren yeager"
+        description: "Abstraction demonstrate an escape route from reality in depicting images in the artwork.",
+        category: "Abstract",
+        artist: "Leslie Gordon"
     },
     {
 
         id:6,
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" ,
+        image: "https://media.istockphoto.com/photos/alcohol-ink-painting-picture-id1161593307?b=1&k=20&m=1161593307&s=170667a&w=0&h=yw39IVkF9ngfcajd5_JuK1FbSdC84QZhzz6t26PV6_Q=" ,
+        
+        description: "This artwork is characterized by an innovative and versatile use of imagery, textures, and mark-making.",
+        category: "Abstract",
+        artist: "Joel Roads"
+    },
+    {
+
+        id:7,
+        image: "https://i.pinimg.com/originals/89/39/a3/8939a3f77359c061870e1a3eea736637.jpg" ,
+        
+        description: "Dark artistic expression freed from the strict Christian dogma.",
+        category: "Dark",
+        artist: "Logan Spacey"
+    },
+    {
+
+        id:8,
+        image: "https://w0.peakpx.com/wallpaper/690/358/HD-wallpaper-the-birth-of-the-legend-mystery-dark-art-ocean-its-so-cool-neosurrealimart-graphics-black-fantasy-ghost-ship-ship-george-grie-dark-night.jpg" ,
+        
+        description: "Great body of work with the morbid and obscure thematic.",
+        category: "Dark",
+        artist: "Jimmie Fields"
+    },
+    {
+
+        id:9,
+        image: "https://www.artroomgalleryonline.com/img/portfolio/2022/Black%20&%20White/Miwa%20Block%20-%20%20Panic%20Attack%20-(acrylic%20on%20canvas)%20www.miwablock.com.jpg" ,
+        
+        description: "The artist transmits his thoughts in a very mysterious way and, occasionally, combining surrealist elements.",
+        category: "Dark",
+        artist: "Leo Adams"
+    },
+    {
+
+        id:10,
+        image: "https://i.pinimg.com/736x/78/bc/cc/78bccc0ad8a39b9bfc491c589714cdf5.jpg" ,
+        
+        description: " The artist transmits his thoughts in a very mysterious way and, occasionally, combining surrealist elements.",
+        category: "Dark",
+        artist: "Josh"
+    },
+    {
+
+        id:11,
+        image: "https://media.fuzia.com/assets/uploads/images/co_brand_1/article/2020/unnamed-1599818866.png" ,
+        
+        description: "Characterized by bright colors, sharp brushstrokes, and quality lines.",
+        category: "Acrylic",
+        artist: "Stuart Miller"
+    },
+    {
+
+        id:12,
+        image: "https://thumbs.dreamstime.com/b/abstract-colorful-oil-acrylic-painting-bird-spring-flower-modern-art-paintings-brush-stroke-canvas-illustration-animal-174299937.jpg" ,
+        
+        description: "Rich painting that has both the soft- and hard-textured elements.",
+        category: "Acrylic",
+        artist: "Casey Ray"
+    },
+    {
+
+        id:13,
+        image: "https://64.media.tumblr.com/b5d576cf92242c725743cb918fe4d650/c4241838024ecb89-97/s540x810/41e882c860eb1055d371feb07569625ed86a23c8.jpg" ,
+        
+        description: "Visual art that captures the expression of ideas and emotions on a two-dimensional surface.",
+        category: "Acrylic",
+        artist: "Eren Yeager"
+    },
+    {
+
+        id:14,
+        image: "https://harunmudak.com/wp-content/uploads/2020/04/easy-acrylic-painting-ideas-simple-1.jpg" ,
+        
+        description: "Rich painting that has both the soft- and hard-textured elements.",
+        category: "Acrylic",
+        artist: "Casey Ray"
+    },
+    {
+
+        id:15,
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/golden-retriever-marion-rose.jpg" ,
+        
+        description: "Visual art that captures the expression of ideas and emotions on a two-dimensional surface.",
+        category: "Acrylic",
+        artist: "Darryl Bolt"
+    },
+    {
+
+        id:16,
+        image: "https://live.staticflickr.com/5527/11447244196_8439e57525.jpg" ,
         
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
-        category: "water",
+        category: "Sketch",
+        artist: "Alex Jay"
+    },
+    {
+
+        id:17,
+        image: "https://i.pinimg.com/originals/16/d4/dc/16d4dcfb88f6291c7949100dc75335ec.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Digital",
         artist: "eren yeager"
-    }
+    },
+    {
+
+        id:18,
+        image: "https://t4.ftcdn.net/jpg/02/09/22/37/360_F_209223706_O8ZlI9qbvZeemjfk35fNN96PlHQUDPkS.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Digital",
+        artist: "Alex Jay"
+    },
+    {
+
+        id:19,
+        image: "https://wallpapers.com/images/file/your-name-anime-digital-art-179tb5mna7wol4k6-179tb5mna7wol4k6.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Digital",
+        artist: "eren yeager"
+    },
+    {
+
+        id:21,
+        image: "https://images.saatchiart.com/saatchi/1782593/art/8402715/7467034-HGMUDYUC-6.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Acrylic",
+        artist: "eren yeager"
+    },
+    {
+
+        id:22,
+        image: "https://images.saatchiart.com/saatchi/1340813/art/7484169/6553121-EEGCGRND-7.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Modern",
+        artist: "eren yeager"
+    },
+    {
+
+        id:23,
+        image: "https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/303/5861/Modern130__45818__63914.1506573857.jpg?c=2" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Modern",
+        artist: "eren yeager"
+    },
+    {
+
+        id:24,
+        image: "https://indianartideas.in/articleimages/1634197208Modern%20Paintings.png" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Modern",
+        artist: "eren yeager"
+    },
+    {
+
+        id:25,
+        image: "https://images.jdmagicbox.com/rep/b2b/modern-art-paintings/modern-art-paintings-3.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Modern",
+        artist: "eren yeager"
+    },
+    {
+
+        id:26,
+        image: "https://images.squarespace-cdn.com/content/v1/506a029784aeea7e77f0b20c/1620055123222-9E33ZVT5NMJ0LUQ4G2HH/Large+abstract+wall+art+naples+fl+artist+Timothy+Parker.jpg?format=2500w" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Modern",
+        artist: "eren yeager"
+    },
+    {
+
+        id:27,
+        image: "https://img.freepik.com/premium-vector/original-sketch-hand-drawing-rome-italy-famous-cityscape-travel-card-vector-illustration_88791-717.jpg?w=2000" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Sketch",
+        artist: "eren yeager"
+    },
+    {
+
+        id:28,
+        image: "https://beautifuldawndesigns.net/wp-content/uploads/2021/03/watercolor-animal-paintings-16.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Watercolour",
+        artist: "eren yeager"
+    },
+    {
+
+        id:29,
+        image: "https://i.pinimg.com/originals/af/b2/73/afb27383cb83070f6a72267c1ac1f145.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Watercolour",
+        artist: "eren yeager"
+    },
+    {
+
+        id:30,
+        image: "https://i.pinimg.com/originals/5e/13/2f/5e132f1d3188a8f856e2017dade17b59.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Watercolour",
+        artist: "eren yeager"
+    },
+    {
+
+        id:31,
+        image: "https://s32625.pcdn.co/wp-content/uploads/2021/02/Close-to-Home-by-Thomas-W-Schaller_WO-758x1024.jpg.optimal.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Watercolour",
+        artist: "eren yeager"
+    },
+    {
+
+        id:32,
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/eiffel-tower-paris-watercolor-mata.jpg" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Watercolour",
+        artist: "eren yeager"
+    },
+    {
+
+        id:33,
+        image: "https://img.freepik.com/premium-vector/dome-st-paul-s-cathedral-london-sketch-beige-background_168440-318.jpg?w=2000" ,
+        
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tempore.",
+        category: "Sketch",
+        artist: "eren yeager"
+    },
 ]
