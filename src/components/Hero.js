786c1b37fbbee7e667dd9e7c7729f6c163c5d403
Shuvabrata Das <102206254+ShuvabrataDas@users.noreply.github.com>
2022-11-06 09:08:33 +0530

    ole

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -27,28 +27,28 @@ function Hero() {
             <div class="border-[4px] border-[#b5892c] rounded-xl shadow-2xl shadow-slate-800 pb-[13px] pt-[35px]  ml-[200px] mr-[200px] ">
                 <div class="flex justify-start items-center pb-32">
                     <hr class="w-[250px] bg-[#b5892c] h-1"/>
-                    <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/eiffel-tower-paris-watercolor-mata.jpg " class="h-[500px] outline-double outline-8 outline-[#b5892c]" alt="Painting"/>
+                    <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/eiffel-tower-paris-watercolor-mata.jpg " class="h-[500px] outline-double outline-8 outline-[#b5892c] hover:opacity-60 transition delay-100" alt="Painting"/>
                     <p class="w-[700px] text-center text-xl font-light font-serif text-[#b5892c] italic pl-5">Lorem ipsum dolor sit, amet consectetur adipisicing
                     elit. Beatae, vero. Deserunt id veritatis voluptates laboriosam.elit. Beatae, vero. Deserunt i
                     Beatae, vero. Deserunt id ve</p>
                 </div> 
                 <div class="flex justify-start items-center pb-32">
                     <hr class="w-[250px] bg-[#b5892c] h-1"/>
-                    <img src="https://images.saatchiart.com/saatchi/1782593/art/8402715/7467034-HGMUDYUC-6.jpg" class="h-[500px] outline-double outline-8 outline-[#b5892c]" alt="Painting"/>
+                    <img src="https://images.saatchiart.com/saatchi/1782593/art/8402715/7467034-HGMUDYUC-6.jpg" class="h-[500px] outline-double outline-8 outline-[#b5892c]  hover:opacity-60 transition delay-100" alt="Painting"/>
                     <p class="w-[700px] text-center text-xl font-light font-serif text-[#b5892c] italic pl-5">Lorem ipsum dolor sit, amet consectetur adipisicing
                     elit. Beatae, vero. Deserunt id veritatis voluptates laboriosam.elit. Beatae, vero. Deserunt i
                     Beatae, vero. Deserunt id ve</p>
                 </div> 
                 <div class="flex justify-start items-center pb-32">
                     <hr class="w-[250px] bg-[#b5892c] h-1"/>
-                    <img src="https://images.jdmagicbox.com/rep/b2b/modern-art-paintings/modern-art-paintings-3.jpg" class="h-[500px] outline-double outline-8 outline-[#b5892c] " alt="Painting"/>
+                    <img src="https://images.jdmagicbox.com/rep/b2b/modern-art-paintings/modern-art-paintings-3.jpg" class="h-[500px] outline-double outline-8 outline-[#b5892c]  hover:opacity-60 transition delay-100" alt="Painting"/>
                     <p class="w-[700px] text-center text-xl font-light font-serif text-[#b5892c] italic pl-5">Lorem ipsum dolor sit, amet consectetur adipisicing
                     elit. Beatae, vero. Deserunt id veritatis voluptates laboriosam.elit. Beatae, vero. Deserunt i
                     Beatae, vero. Deserunt id ve</p>
                 </div> 
                 <div class="flex justify-start items-center  pb-10">
                     <hr class="w-[250px] bg-[#b5892c] h-1"/>
-                    <img src="https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/303/5861/Modern130__45818__63914.1506573857.jpg?c=" class="h-[500px] outline-double outline-8 outline-[#b5892c]" alt="Painting"/>
+                    <img src="https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/303/5861/Modern130__45818__63914.1506573857.jpg?c=" class="h-[500px] outline-double outline-8 outline-[#b5892c]  hover:opacity-60 transition delay-100" alt="Painting"/>
                     <p class="w-[700px] text-center text-xl font-light font-serif text-[#b5892c] italic pl-5">Lorem ipsum dolor sit, amet consectetur adipisicing
                     elit. Beatae, vero. Deserunt id veritatis voluptates laboriosam.elit. Beatae, vero. Deserunt i
                     Beatae, vero. Deserunt id ve</p>
