22aed313ec826fbf05badc65bad2a29180510158
Jyotishree <jyotishreeb007@gmail.com>
2022-11-06 19:54:43 +0530

    gallery edited

--- src/Gallery.js
+++ src/Gallery.js
@@ -26,66 +26,73 @@ function Gallery() {
     <>
     <Navbar2/>
     {/* intro */}
-
-    <div className='text-4xl text-center my-20'>
-      GALLERY 
-    </div>
-
-    <div className='flex flex-row justify-center space-x-10 mb-10'>
-      <button className='border-2 p-3 bg-black text-white' onClick={()=> filterItem('oil')}>OIL</button>
-      <button className='border-2 p-3 bg-black text-white' onClick={()=> filterItem('water')}>WATER</button>
-      <button className='border-2 p-3 bg-black text-white'onClick={()=> filterItem('dark')}>DARK</button>
-      <button className='border-2 p-3 bg-black text-white'onClick={()=> setArt(gallery)}>All</button>
-      
-    </div>
-
-      
-    <div className='grid grid-cols-4 gap-3'>
-    {
-      art.map((elem)=>{
-
-        return(  
-          <>
-          <div className='flex flex-col gap-x-10'>
-          <button onClick={()=>{ changeContent([elem])}}>
-          <img src={elem.image} alt="" className='h-96' />
-          </button>
-          
-          <p>{elem.artist}</p>
-          <p>{elem.description}</p>
-          </div>
-          </>
-        )
-      })
-    }
-    </div>
-
-    {/* popUp Container */}
-
-    { content && 
-    
-    <div onClick={changeContent}   className='fixed top-0 flex items-center justify-center p-10 left-0 right-0 bottom-0 bg-opacity-25 bg-black z-10'>
-    <div className='bg-white p-10 rounded-lg max-w-2xl z-50 relative'>
-      <button onClick={changeContent} className="bg-yellow-500">x</button>
-        {
-          modal.map((pop) => {
-
-            return(
-              <>
-            <h1>{pop.id}</h1>
-            <h1>{pop.artist}</h1>
-            <h1>{pop.description}</h1>
-            </>
-            )
-          })
-        }
-
+    <div class='bg-[#b9b4f7]'>
+      <div class="flex justify-center items-center space-x-12 py-24">
+          <hr class="w-32 bg-[#383838] h-0.5"/>
+          <p class="text-[#383838] text-5xl py-6 font-serif font-light">GALLERY</p>
+          <hr class="w-32  bg-[#383838] h-0.5"/>
       </div>
 
+      <div className='flex flex-row justify-center space-x-8 mb-10'>
+        <p class="text-[#525252] text-xl py-3 font-serif font-light">CATAGORIES :</p>
+        <button className='border-2 p-3 bg-black text-white rounded-3xl' onClick={()=> filterItem('Watercolour')}>WATERCOLOUR</button>
+        <button className='border-2 p-3 bg-black text-white rounded-3xl' onClick={()=> filterItem('Acrylic')}>ACRYLIC</button>
+        <button className='border-2 p-3 bg-black text-white rounded-3xl'onClick={()=> filterItem('Dark')}>DARK</button>
+        <button className='border-2 p-3 bg-black text-white rounded-3xl' onClick={()=> filterItem('Sketch')}>SKETCH</button>
+        <button className='border-2 p-3 bg-black text-white rounded-3xl' onClick={()=> filterItem('Digital')}>DIGITAL</button>
+        <button className='border-2 p-3 bg-black text-white rounded-3xl' onClick={()=> filterItem('Modern')}>MODERN</button>
+        <button className='border-2 p-3 bg-black text-white rounded-3xl' onClick={()=> filterItem('Abstract')}>ABSTRACT</button>
+        <button className='border-2 py-3 px-5 bg-black text-white rounded-3xl'onClick={()=> setArt(gallery)}>ALL</button>
+        
+      </div>
 
-    </div>}
+        
+      <div className='grid grid-cols-4 gap-4 mx-9'>
+      {
+        art.map((elem)=>{
 
-    
+          return(  
+            <>
+            <div className='flex flex-col gap-x-10'>
+            <button onClick={()=>{ changeContent([elem])}}>
+            <img src={elem.image} alt="" className='h-96 w-full rounded-lg border-[#606061]' />
+            </button>
+            
+            {/* <p>{elem.artist}</p>
+            <p>{elem.description}</p> */}
+            </div>
+            </>
+          )
+        })
+      }
+      </div>
+
+      {/* popUp Container */}
+
+      { content && 
+      
+      <div onClick={changeContent}   className='fixed top-0 flex items-center justify-center p-10 left-0 right-0 bottom-0 bg-opacity-30 bg-black z-10 backdrop-blur-sm '>
+      <div className='bg-white p-10 rounded-lg max-w-2xl z-50 relative'>
+        <button onClick={changeContent} className="bg-yellow-500 h-3 w-5 pb-7">x</button>
+          {
+            modal.map((pop) => {
+
+              return(
+                <>
+              <h2>{pop.id}</h2>
+              <h2>{pop.artist}</h2>
+              <h2>{pop.description}</h2>
+              </>
+              )
+            })
+          }
+
+        </div>
+
+
+        </div>}
+
+    </div>
     
     </>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     
-    <div className='bg-transparent fixed backdrop-blur-lg w-full'>
+    <div className='bg-transparent fixed backdrop-blur-lg w-full lg:h-20'>
     {/* NAVBAR */}
-    <nav class="relative container mx-auto p-2">
+    <nav class="relative container mx-auto p-0.5">
        {/* FLEX CONTAINER */}
          <div class="flex items-center justify-between">
             {/* logo */}
@@ -19,10 +19,10 @@ function Navbar() {
           </div>
           {/* menu */}
          <div class="hidden items-center md:flex flex-auto space-x-12">
-         <Link to="/"><div class ="text-2xl ml-5 text-white hover:text-[#34d1c4] cursor-pointer  hover:underline duration-1000" id="tol">HOME</div></Link>
-         <Link to="/about"><div class ="text-2xl text-white hover:text-[#34d1c4] cursor-pointer  hover:underline duration-1000" id="tol">ABOUT US</div></Link>
-         <Link to="/gallery"><div class ="text-2xl text-white hover:text-[#34d1c4] cursor-pointer  hover:underline duration-1000" id="tol">GALLERY</div></Link>
-         <Link to="/contact"><div class ="text-2xl text-white hover:text-[#34d1c4] cursor-pointer  hover:underline duration-1000" id="tol">CONTACT US</div></Link>
+         <Link to="/"><div class ="text-xl ml-5 text-white hover:text-[#34d1c4] cursor-pointer  hover:underline duration-1000" id="tol">HOME</div></Link>
+         <Link to="/about"><div class ="text-xl text-white hover:text-[#34d1c4] cursor-pointer  hover:underline duration-1000" id="tol">ABOUT US</div></Link>
+         <Link to="/gallery"><div class ="text-xl text-white hover:text-[#34d1c4] cursor-pointer  hover:underline duration-1000" id="tol">GALLERY</div></Link>
+         <Link to="/contact"><div class ="text-xl text-white hover:text-[#34d1c4] cursor-pointer  hover:underline duration-1000" id="tol">CONTACT US</div></Link>
            
          </div>
 
