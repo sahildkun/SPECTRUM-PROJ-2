36b5798bd8a45f90ce6e04f341c4810e593f6100
Jyotishree <jyotishreeb007@gmail.com>
2022-11-08 19:12:46 +0530

    hero fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     
-    <div className='bg-transparent fixed backdrop-blur-lg w-full lg:h-20'>
+    <div className='bg-black bg-opacity-20 fixed  backdrop-filter backdrop-blur-lg w-full lg:h-20'>
     {/* NAVBAR */}
     <nav class="relative container mx-auto p-0.5">
        {/* FLEX CONTAINER */}
@@ -13,16 +13,16 @@ function Navbar() {
             {/* logo */}
           <div className='py-0.5 w-28 mx-auto lg:mx-5'>
            <Link to='/'>
-           <img src="logo2.png" alt="logo-img"/>
+           <img src="logo.png" alt="logo-img"/>
            </Link>
                
           </div>
           {/* menu */}
          <div class="hidden items-center md:flex flex-auto space-x-12">
-         <Link to="/"><div class ="text-xl ml-5 text-black hover:text-[#f5f8f8] cursor-pointer  hover:underline duration-1000" id="tol">HOME</div></Link>
-         <Link to="/about"><div class ="text-xl text-black hover:text-[#f5f8f8] cursor-pointer  hover:underline duration-1000" id="tol">ABOUT US</div></Link>
-         <Link to="/gallery"><div class ="text-xl text-black hover:text-[#f5f8f8] cursor-pointer  hover:underline duration-1000" id="tol">GALLERY</div></Link>
-         <Link to="/contact"><div class ="text-xl text-black hover:text-[#f5f8f8] cursor-pointer  hover:underline duration-1000" id="tol">CONTACT US</div></Link>
+         <Link to="/"><div class ="text-xl ml-5 hover:text-[#fdc751] text-[#f5f8f8] cursor-pointer  hover:underline duration-1000" id="tol">HOME</div></Link>
+         <Link to="/about"><div class ="text-xl hover:text-[#fdc751]  text-[#f5f8f8] cursor-pointer  hover:underline duration-1000" id="tol">ABOUT US</div></Link>
+         <Link to="/gallery"><div class ="text-xl hover:text-[#fdc751]  text-[#f5f8f8] cursor-pointer  hover:underline duration-1000" id="tol">GALLERY</div></Link>
+         <Link to="/contact"><div class ="text-xl hover:text-[#fdc751]  text-[#f5f8f8] cursor-pointer  hover:underline duration-1000" id="tol">CONTACT US</div></Link>
            
          </div>
 
